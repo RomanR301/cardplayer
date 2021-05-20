08683a3be35f55c0644fa24f5cffb59de303bd5b
Roman <roman.sav15@gmail.com>
2021-05-20 12:45:54 +0300

    blog author blog detail faq pages, small fixes

--- #src/js/script.js
+++ #src/js/script.js
@@ -104,28 +104,3 @@ document.addEventListener("DOMContentLoaded", function (event) {
 jQuery(function () {
   front.init();
 });
-
-$(function() {
-  
-  $('.dropdown > .caption').on('click', function() {
-    $(this).parent().toggleClass('open');
-  });
-  
-  $('.dropdown .tabs-list .tabs-list__item').on('click', function() {
-    $('.dropdown .tabs-list .tabs-list__item').removeClass('selected');
-    $(this).addClass('selected').parent().parent().removeClass('open').children('.caption').text( $(this).text() );
-  });
-  
-  $(document).on('keyup', function(evt) {
-    if ( (evt.keyCode || evt.which) === 27 ) {
-      $('.dropdown').removeClass('open');
-    }
-  });
-  
-  $(document).on('click', function(evt) {
-    if ( $(evt.target).closest(".dropdown > .caption").length === 0 ) {
-      $('.dropdown').removeClass('open');
-    }
-  });
-  
-});
